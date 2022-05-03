ec550fe72d22f8e18f66dc5b16fd8f91a7853178
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-04 01:36:45 +0300

    first commit

---
+++ test.rb
@@ -0,0 +1,12 @@
+# typr "ruby filename" to run
+
+# print "hey"
+box = 34
+name = 'Panos'
+friends = ['Nokio','Nik','Meloyan']
+
+
+
+# mostly use puts, p is used when you want to debug cause it gives you more information
+puts friends
+p friends
