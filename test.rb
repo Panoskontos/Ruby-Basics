7b4e61c64b9d8981182293595270b4cb02938237
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-04 01:40:37 +0300

    first commit

--- test.rb
+++ test.rb
@@ -5,8 +5,10 @@ box = 34
 name = 'Panos'
 friends = ['Nokio','Nik','Meloyan']
 
-
+# friends = "change type"
 
 # mostly use puts, p is used when you want to debug cause it gives you more information
 puts friends
 p friends
+
+
